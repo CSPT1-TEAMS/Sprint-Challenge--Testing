aebaaf8c33421942d04462fdaa3e48b1b9459758
Andrew Jarrett <ahrjarrett@gmail.com>
2018-11-22 21:21:25 -0600

    complete POST tests; begin GET tests

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
+const request = require('supertest')
 
+const server = require('./server')
 const Game = require('../games/Game')
 
 describe('The API Server', () => {
@@ -19,44 +21,81 @@ describe('The API Server', () => {
   })
 
   let gameId
+  let savedGames
+
+  let testGames = {
+    games: [
+      {
+        title: 'X-Men vs. Street Fighter',
+        genre: 'Fighting',
+        releaseDate: 'September 25, 1996',
+      },
+      {
+        title: 'Marvel Super Heroes vs. Street Fighter',
+        genre: 'Fighting',
+        releaseDate: 'October 22, 1998',
+      },
+      {
+        title: 'Marvel vs. Capcom: Clash of the Super Heroes',
+        genre: 'Fighting',
+        releaseDate: 'January 23, 1998',
+      },
+    ]
+  }
+
+  let goodGame = {
+    title: 'Ultimate Marvel vs. Capcom 3',
+    genre: 'Fighting',
+    releaseDate: 'November 15, 2011',
+  }
+  let badGame = {
+    genre: 'Fighting',
+    releaseDate: 'November 15, 2011',
+  }
 
   beforeEach(async () => {
-    //   // write a beforeEach hook that will populate your test DB with data
-    //   // each time this hook runs, you should save a document to your db
-    //   // by saving the document you'll be able to use it in each of your `it` blocks
-
-    const testGames = {
-      games: [
-        {
-          title: 'X-Men vs. Street Fighter',
-          genre: 'Fighting',
-          releaseDate: 'September 25, 1996',
-        },
-        {
-          title: 'Marvel Super Heroes vs. Street Fighter',
-          genre: 'Fighting',
-          releaseDate: 'October 22, 1998',
-        },
-        {
-          title: 'Marvel vs. Capcom: Clash of the Super Heroes',
-          genre: 'Fighting',
-          releaseDate: 'January 23, 1998',
-        },
-      ]
-    }
-
-    let savedGames = await Promise.all(testGames.games.map(game => Game.create(game)))
-    console.log(savedGames)
+    savedGames = await Promise.all(testGames.games.map(game => Game.create(game)))
   })
 
-  afterEach(() => {
+  afterEach(() => Game.remove({}))
+
+  describe('POST requests', () => {
+    it('Posts a correctly formatted game', async () => {
+      await request(server)
+        .post(`/api/games`)
+        .send(goodGame)
+        .expect('Content-Type', /json/)
+        .expect(201)
+    })
+
+    it('Post route returns json', async () => {
+      await request(server)
+        .post(`/api/games`)
+        .send(goodGame)
+        .expect('Content-Type', /json/)
+    })
+
+    it('Returns an error with badly formatted data', async () => {
+      await request(server)
+        .post(`/api/games`)
+        .send(badGame)
+        .expect(500)
+    })
   })
 
-  it('runs the tests', () => { })
+  describe('GET requests', () => {
+    it('Returns a status of 200', async () => {
+      await request(server)
+        .get(`/api/games`)
+        .expect(200)
+    })
 
-  // test the POST here
+    it('Returns the list of games', async () => {
+      const games = await request(server)
+        .get(`/api/games`)
 
-  // test the GET here
+      console.log(games)
+    })
 
-  // Test the DELETE here
+  })
 })
